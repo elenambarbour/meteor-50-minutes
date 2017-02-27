ed50f23ce762825d571780731e3083c44d75d599
elenambarbour <ebarbour@hawaii.edu>
2017-02-26 22:17:45 -1000

    Task 3: Tasklist Form

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,4 +1,3 @@
-
 /**
  * Created by owner on 2/26/2017.
  */
@@ -7,8 +6,29 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function(){
-      return Tasks.find({}, {sort: {createdAt: -1}});
+    tasks: function () {
+      return Tasks.find({}, { sort: { createdAt: -1 } });
+    }
+  });
+  Template.tasks.events({
+    "submit .add-task": function (event) {
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = '';
+
+      return false;
+    },
+    "click .delete-task": function (event) {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+
+      return false;
     }
   })
 }
