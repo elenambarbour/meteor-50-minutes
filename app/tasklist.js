a422e16e8f175f1aa9db6ea989ae4c6b15fd7620
elenambarbour <ebarbour@hawaii.edu>
2017-02-26 22:02:56 -1000

    Task 2: Tasklist Setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+
+/**
+ * Created by owner on 2/26/2017.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  })
+}
+
+if (Meteor.isServer) {
+
+}
